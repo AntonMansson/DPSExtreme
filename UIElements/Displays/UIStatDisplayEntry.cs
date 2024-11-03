6017fadd52a1bdd8cf0dc1a15c86155560be24a2
AntonMansson <Anton_mansson@hotmail.se>
2024-11-03 17:11:04 +0100

    Format stat numbers

--- UIElements/Displays/UIStatDisplayEntry.cs
+++ UIElements/Displays/UIStatDisplayEntry.cs
@@ -21,11 +21,25 @@ namespace DPSExtreme.UIElements.Displays
 			return (int)(GetOuterDimensions().Width * (myValue / myMax));
 		}
 
+		private string FormatStatNumber(int aValue)
+		{
+			if (aValue >= 100000000) 
+				return FormatStatNumber(aValue / 1000000) + "M";
+
+			if (aValue >= 100000)
+				return FormatStatNumber(aValue / 1000) + "K";
+
+			if (aValue >= 10000)
+				return (aValue / 1000D).ToString("0.#") + "K";
+
+			return aValue.ToString("#,0");
+		}
+
 		protected override void DrawSelf(SpriteBatch spriteBatch)
 		{
 			myRightText = myValue.ToString();
 			if (DPSExtremeUI.instance.myShowPercent && myTotal > 0)
-				myRightText = $"{myValue} ({String.Format("{0:P0}", (float)myValue / myTotal)})";
+				myRightText = $"{FormatStatNumber(myValue)} ({String.Format("{0:P0}", (float)myValue / myTotal)})";
 
 			DrawSelfBase(spriteBatch);
 
